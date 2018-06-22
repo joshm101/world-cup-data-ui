97fa1ec53ef789ea2c9cf1437f4855416b9d519b
Joshua Mora <jshm101@gmail.com>
2018-06-21 23:21:42 -0700

    Prevent app from running when REACT_APP_WCDUI_API_URL is not provided

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import GroupStandings from './components/GroupStandings'
+
 class App extends Component {
   render() {
+    const API_URL = process.env.REACT_APP_WCDUI_API_URL
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {API_URL &&
+          <div />
+        }
+        {!API_URL &&
+          <h2>No API URL found. This application cannot run.</h2>
+        }
       </div>
     );
   }
